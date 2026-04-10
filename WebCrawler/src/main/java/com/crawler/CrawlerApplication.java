a446d697c858a2d5ff53daa796dccf9a2a14cd29
彭安民 <2332045320@qq.com>
2026-04-10 15:05:44 +0800

    配置mysql数据库、redis数据库、以及启动类

--- WebCrawler/src/main/java/com/crawler/CrawlerApplication.java
+++ WebCrawler/src/main/java/com/crawler/CrawlerApplication.java
@@ -1,4 +1,13 @@
 package com.crawler;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.crawler.mapper")
 public class CrawlerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApplication.class, args);
+    }
 }
