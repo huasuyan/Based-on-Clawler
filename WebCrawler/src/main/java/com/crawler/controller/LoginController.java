3a50a2d35cf61a7348e3eef3e5eb77b9c2245142
胡佳荣 <873981169@qq.com>
2026-04-10 17:49:24 +0800

    登录接口

--- WebCrawler/src/main/java/com/crawler/controller/LoginController.java
+++ WebCrawler/src/main/java/com/crawler/controller/LoginController.java
@@ -1,31 +1,68 @@
 package com.crawler.controller;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.core.codec.Base64;
+import cn.hutool.core.io.FastByteArrayOutputStream;
+import cn.hutool.core.util.IdUtil;
 import com.crawler.entity.LoginDto;
 import com.crawler.entity.Result;
 import com.crawler.entity.User;
 import com.crawler.service.LoginService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Slf4j
+
+
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class LoginController {
+    //注入RedisTemplate
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
     private LoginService loginService;
 
+    /**
+     * 获取验证码
+     */
+    @GetMapping("/getCode")
+    public Result getCode() {
+        // 生成验证码图片
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+        String verify = IdUtil.simpleUUID();
+        String code = lineCaptcha.getCode();
+        // 输出图片流
+        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
+        lineCaptcha.write(os);
+        //存入Redis，有效期1分钟
+        redisTemplate.opsForValue().set("captcha:" + verify, code, 60L, TimeUnit.SECONDS);
+        // 返回前端
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>(5);
+        map.put("uuid", verify);
+        map.put("img", Base64.encode(os.toByteArray()));
+        return Result.success(map);
+    }
+
     @PostMapping("login")
     public Result login(@RequestBody User user) {
-        log.info("登录:{}",user);
+
         LoginDto info = loginService.login(user);
 
-        if(info!=null){
+        if (info != null) {
             return Result.success();
         }
         return Result.error("用户名或密码错误");
     }
+
 }
