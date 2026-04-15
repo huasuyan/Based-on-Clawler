c2cf133fe3360f8be2f885cfd45ca4266cbd7832
胡佳荣 <873981169@qq.com>
2026-04-15 14:56:11 +0800

    新增爬虫管理框架

---
+++ WebCrawler/src/main/java/com/crawler/controller/CrawlerController.java
@@ -0,0 +1,10 @@
+package com.crawler.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/crawler")
+public class CrawlerController {
+
+
+}
---
+++ WebCrawler/src/main/java/com/crawler/service/impl/CrawlerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.crawler.service.impl;
+
+import com.crawler.service.CrawlerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlerServiceImpl implements CrawlerService {
+
+
+}
