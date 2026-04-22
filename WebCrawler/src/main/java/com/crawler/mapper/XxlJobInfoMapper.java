4eb88467c91dd3ca0630bf1d613c48d6d70e71f8
hw-huasuyan 陈淇 <qazwsx15975382460@163.com>
2026-04-22 18:16:47 +0800

    删除Crawler和XXL-job类

--- WebCrawler/src/main/java/com/crawler/mapper/XxlJobInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.crawler.mapper;
-
-import com.crawler.entity.xxljob.XxlJobInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface XxlJobInfoMapper {
-    XxlJobInfo selectByJobId(Integer jobId);
-}
