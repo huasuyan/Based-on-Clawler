27c8e8b92e724b34cbb1e58170986893328a2150
彭安民 <2332045320@qq.com>
2026-04-27 14:19:31 +0800

    完成/dept/tree接口的部门参数设置

--- WebCrawler/src/main/java/com/crawler/controller/DeptController.java
+++ WebCrawler/src/main/java/com/crawler/controller/DeptController.java
@@ -4,7 +4,11 @@ package com.crawler.controller;
 import com.crawler.annotation.RequirePermission;
 import com.crawler.entity.Dept;
 import com.crawler.entity.Result;
+import com.crawler.entity.Role;
 import com.crawler.entity.User;
+import com.crawler.mapper.DeptMapper;
+import com.crawler.mapper.RoleMapper;
+import com.crawler.mapper.UserRoleMapper;
 import com.crawler.service.DeptService;
 import com.crawler.service.DeptUserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,18 +24,38 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
     @Autowired
-    private DeptUserService deptUserService;
+    private RoleMapper roleMapper;
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+    @Autowired
+    private DeptMapper deptMapper;
 
     @GetMapping("/tree")
     public Result getDeptTree(HttpServletRequest request,@RequestParam Integer showEnable) {
         User currentUser = (User) request.getAttribute("currentUser");
 
         // TODO 权限
-        if(deptService.getDeptTree(currentUser.getDeptId(), showEnable) == null) {
+        // 根据用户权限设置top部门ID
+        Long topDeptId = currentUser.getDeptId();
+        Long roleId = userRoleMapper.selectRoleIdByUserId(currentUser.getUserId());
+        Role role = roleMapper.selectById(roleId);
+        Integer dataScope = role.getDataScope();
+        if (dataScope == 1) {
+            topDeptId = currentUser.getDeptId();
+        }else if (dataScope == 2) {
+            topDeptId = deptMapper.getTopLevelParentId(currentUser.getDeptId());
+        }else if (dataScope == 3) {
+            topDeptId = 0L;
+        }else{
+            throw new RuntimeException("未知的数据权限范围！");
+        }
+
+
+        if(deptService.getDeptTree(topDeptId, showEnable) == null) {
             return Result.error("部门树不存在");
         }
 
-        return deptService.getDeptTree(currentUser.getDeptId(), showEnable);
+        return deptService.getDeptTree(topDeptId, showEnable);
     }
 
     /**
